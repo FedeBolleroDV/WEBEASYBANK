c7f14d6942bad0a64e308e778befc7b41faed395
FedeBolleroDV <fede.bollero@gmail.com>
2021-01-21 17:25:45 -0300

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+
+btnHamburger.addEventListener("click", function () {
+    console.log("open hamburger");
+
+    if (header.classList.contains("open")) {
+        header.classList.remove("open");
+    }
+    else {
+        header.classList.add("open");
+    }
+});
